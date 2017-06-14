fff3c1b9a1cfcc3e12e130e17e65b182c92be136
detamos <deepanshu_tyagi@students.iitmandi.ac.in>
2017-06-14 14:50:05 +0530

    stdlib.h added

--- include/mergeSort.h
+++ include/mergeSort.h
@@ -3,8 +3,11 @@
 
 #include <stdio.h>
 #include <limits.h>
+#include <stdlib.h>
 
 void merge(long *,long ,long ,long );
 void mergeSort(long *,long ,long );
+
+void mergeSort_omp(long *arr,long l,long r);
 
 #endif
